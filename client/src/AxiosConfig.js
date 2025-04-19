a9b12ede7fd0c6fadfd2e9bd0cc2b4c245a38563
dhansikasethi34829 <dhansikasethi34829@iisuniv.ac.in>
2025-04-19 21:42:47 +0530

    Third commit

--- client/src/AxiosConfig.js
+++ client/src/AxiosConfig.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const Instance = axios.create({
     // baseURL: `https://stock-menegement-back.onrender.com`,
-    // baseURL: "http://localhost:8080/",
-    baseURL: "https://iisu-inventory-management-system-back.onrender.com",
+    baseURL: "http://localhost:8080/",
+    // baseURL: "https://iisu-inventory-management-system-back.onrender.com",
     withCredentials:true,
 });
 
